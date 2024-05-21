8e76802ac32a876508651f0e306be787eec7e26d
Théo Gianella <theo.gianella@proton.me>
2024-05-21 22:50:16 +0200

    Added new placeholders on input

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,8 +1,8 @@
 export const Form = ({onSubmit, actionLabel}) => {
     return (
         <form className="form" onSubmit={onSubmit}>
-            <input name="username" autoComplete="off" />
-            <input name="password" autoComplete="off"/>
+            <input name="username" placeholder="Nom d'utilisateur" autoComplete="off" />
+            <input name="password" placeholder="Mot de passe" autoComplete="off"/>
             <button type="submit">{actionLabel}</button>
         </form>
     )
--- src/TextInput.jsx
+++ src/TextInput.jsx
@@ -3,7 +3,7 @@ import './TextInput.css';
 export const TextInput = ({onSubmit}) => {
     return (
         <form className="password-form" onSubmit={onSubmit}>
-            <input className="password-input" name="plaintext" autoComplete="off" />
+            <input className="password-input" name="plaintext" placeholder="Mot de passe" autoComplete="off" />
             <button type="submit">Envoyer</button>
         </form>
     )
