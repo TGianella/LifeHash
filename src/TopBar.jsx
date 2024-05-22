c9dcb1230587715df72d6ddb663703f0d7d66351
Théo Gianella <theo.gianella@proton.me>
2024-05-22 17:38:07 +0200

    fix topbar

--- src/TopBar.jsx
+++ src/TopBar.jsx
@@ -3,8 +3,9 @@ import './TopBar.css'
 export const TopBar = ({onSubmit, onButtonClick}) => {
     return (
         <div className="top-bar">
+            <div></div>
             <TextInput onSubmit={onSubmit}/>
-            <button onClick={onButtonClick} style={{padding: "10px"}}>Connexion</button>
+            <button onClick={onButtonClick}>Connexion</button>
         </div>
     )
 }
